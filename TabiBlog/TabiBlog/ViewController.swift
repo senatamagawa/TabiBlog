71253ccf327f4bb1c060fc0dd913a02e0685fdc8
SENA TAMAGAWA <sena.web0310@gmail.com>
2024-12-09 23:12:04 +0900

    first commit

---
+++ TabiBlog/TabiBlog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabiBlog
+//
+//  Created by 玉川　世那 on 2024/12/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
